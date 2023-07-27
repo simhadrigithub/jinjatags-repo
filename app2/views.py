87993eff6c08d491481fc0ba06c38f1eeb0ce6d2
simhadrigithub <murugeshsimhadri88888@gmail.com>
2023-07-27 13:50:39 +0530

    first commit

--- app2/views.py
+++ app2/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 
-d={'name':'simha','age':21,'range':100,'frame_work':'django','time':10.35,'from':'chittoor'}
+d={'name':'simha','age':21,'range':100,'frame_work':'django','time':10.35,'from':'chittoor', 'duration':'2hours'}
 
 def render2(request):
     return render(request,'render2.html',context=d)
